6e6501660c2cf6802859c4133e122b42b232b81c
Henrique Nascimento <nascimentocostahenrique@gmail.com>
2017-07-31 10:05:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from kivy.app import App
-from kivy.uix.scatter import Scatter
+from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+class myapp(GridLayout):
+    def __init__(self, **kwargs):
+        super(myapp, self).__init__(**kwargs)
+        self.cols = 2
 
-class MyApp(App):
+        self.add_widget(Label(text='Usuario'))
+        self.usuario = TextInput(multiline=True)
+        self.add_widget(self.usuario)
+
+        self.add_widget(Label(text='Senha'))
+        self.senha = TextInput(password=True, multiline=False)
+        self.add_widget(self.senha)
+
+class runapp(App):
     def build(self):
-        f = FloatLayout()
-        s = Scatter()
-        l = Label(text='Hello world',
-                  font_size=55)
-        f.add_widget(s)
-        s.add_widget(l)
-        return f
-if __name__ == "__main__":
-    MyApp().run()
+        return myapp()
+
+runapp().run()
